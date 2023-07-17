b3496968e093005c78386c70be7287a4de081c4a
Stavros Schizas <stavros.schizas@gmail.com>
2023-07-17 23:10:59 +0300

    httpsimctl 1.0.0

--- Formula/httpsimctl.rb
+++ Formula/httpsimctl.rb
@@ -1,8 +1,8 @@
 class Httpsimctl < Formula
   desc "Command-line tool for interacting with iOS Simulators via the HTTP REST API"
   homepage "https://github.com/sschizas/HTTPsimctl"
-  url "https://github.com/sschizas/HTTPsimctl/archive/refs/tags/0.0.0.tar.gz"
-  sha256 "dfa12be19aeb86674080f41061cd8346bf26aa0cad15dd4d82c46fab678f6fa4"
+  url "https://github.com/sschizas/HTTPsimctl/releases/download/1.0.0/httpsimctl.tar.gz"
+  sha256 "51534220f9b2e286ddafb826871c800212f23d57428444b5224bf5d04727eb39"
   license "MIT"
   head "https://github.com/sschizas/HTTPsimctl.git", branch: "main"
 
